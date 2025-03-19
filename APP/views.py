f5881ed4e7862d164785db92d7d342649782a9b4
arkosarker07 <arkosarker07@gmail.com>
2025-03-19 21:43:35 +0600

    Home

--- APP/views.py
+++ APP/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
+def hello(request):
     return render(request, 'Nigga.html')
+def home(request):
+    return render(request, 'homepage.html')
