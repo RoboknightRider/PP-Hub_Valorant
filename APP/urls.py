de23a21f68a4527b0841d64851fd39769d9a69f5
RoboknightRider <divyajyotibagchi@gmail.com>
2025-03-21 22:33:51 +0600

    Quick Save

--- APP/urls.py
+++ APP/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    #path('logout/', views.logout, name='logout'),
     
 ]
--- APP/views.py
+++ APP/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def home(request):
+    return render(request, 'home.html')
 def register(request):
     return render(request, 'register.html')
 def login(request):
     return render(request, 'login.html')
-def home(request):
-    return render(request, 'home.html')
+def dashboard(request):
+    return render(request, 'dashboard.html')
+def logout(request):
+    return render(request, 'logout.html')
