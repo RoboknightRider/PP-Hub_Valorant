324c0b5e19aa3b130e4c9d1ae204f1c42441d368
arkosarker07 <arkosarker07@gmail.com>
2025-04-30 21:01:41 +0600

    fixed

--- PP-Hub_Valorant-Sage/APP/admin.py
+++ PP-Hub_Valorant-Sage/APP/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import StudentProfile, UploadedFile
+from .models import StudentProfile, UploadedFile, ChatMessage
 
 admin.site.register(StudentProfile)
 admin.site.register(UploadedFile)
+admin.site.register(ChatMessage)
